5bb91730dd97cfb5b22bd6f7873708dd2f6defe9
Dallin Hoyt <dhhoyt2002@gmail.com>
2023-10-17 11:30:35 -0600

    inital commit

---
+++ src/main/java/net/kiwen/App.java
@@ -0,0 +1,13 @@
+package net.kiwen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
